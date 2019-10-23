f7c6d2bc83ed17a70127d34ae1be88f4e283849b
MaartenGr <maarten_grootendorst@hotmail.com>
2019-10-23 09:06:50 +0200

    Update file structure

--- app.py
+++ app.py
@@ -9,16 +9,15 @@ import general as gn
 def main():
     df, player_list = load_data()
 
-    body = " ".join(open("homepage.md", 'r').readlines())
+    body = " ".join(open("markdown/homepage.md", 'r').readlines())
     readme_text = st.markdown(body, unsafe_allow_html=True)
 
-    # readme_text = st.markdown("# test", unsafe_allow_html=True)
     st.sidebar.title("Menu")
     app_mode = st.sidebar.selectbox("Selecteer de modus", ["Homepage", "General",
                                                            "Stats per player", "Compare players"])
     if app_mode == "Instruction":
         readme_text.empty()
-        body = " ".join(open("instructions.md", 'r').readlines())
+        body = " ".join(open("markdown/instructions.md", 'r').readlines())
         st.markdown(body, unsafe_allow_html=True)
     elif app_mode == "General":
         gn.explore_data(df, readme_text)
@@ -31,7 +30,7 @@ def main():
 
 @st.cache
 def load_data():
-    df = pd.read_csv("boardgame.csv")
+    df = pd.read_csv("files/boardgame.csv")
     player_list = df.Players.unique()
     player_list = [players.split('+') for players in player_list]
     player_list = list(set([player for sublist in player_list for player in sublist]))
