961325cb41b9fb09601ec9cd3eb3457145aba885
MaartenGr <maarten_grootendorst@hotmail.com>
2019-10-23 09:17:19 +0200

    Test heroku

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import general as gn
 def main():
     df, player_list = load_data()
 
-    body = " ".join(open("markdown/homepage.md", 'r').readlines())
+    body = " ".join(open("homepage.md", 'r').readlines())
     readme_text = st.markdown(body, unsafe_allow_html=True)
 
     st.sidebar.title("Menu")
@@ -17,7 +17,7 @@ def main():
                                                            "Stats per player", "Compare players"])
     if app_mode == "Instruction":
         readme_text.empty()
-        body = " ".join(open("markdown/instructions.md", 'r').readlines())
+        body = " ".join(open("instructions.md", 'r').readlines())
         st.markdown(body, unsafe_allow_html=True)
     elif app_mode == "General":
         gn.explore_data(df, readme_text)
@@ -30,7 +30,7 @@ def main():
 
 @st.cache
 def load_data():
-    df = pd.read_csv("files/boardgame.csv")
+    df = pd.read_csv("boardgame.csv")
     player_list = df.Players.unique()
     player_list = [players.split('+') for players in player_list]
     player_list = list(set([player for sublist in player_list for player in sublist]))
---
+++ homepage.md
@@ -0,0 +1,18 @@
+# Exploring one year of Board Games
+ 
+> Code and instructions for creating a turnover predicting-API
+
+Choosing between several possible candidates when hiring is a 
+difficult and complex process. One of the questions you might ask
+is whether this applicant is likely to stay at the company one year later. 
+The API predicts the chance of a candidate staying more than one year
+at company and additionally provides the reasoning for that prediction.   
+
+<div align="center">
+  <br>
+  <img src="https://img.shields.io/badge/MADE%20WITH-PYTHON%20-red?style=for-the-badge"
+      alt="API stability" height="30"/>
+  <img src="https://img.shields.io/badge/SERVED%20WITH-Heroku-blue?style=for-the-badge"
+      alt="API stability" height="30"/>
+</div>
+
