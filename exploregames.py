d5ccf51c1b322bd04b6bd051e3f622495f7769c5
Maarten Grootendorst <m.grootendorst@vanspaendonck.nl>
2019-11-04 12:57:43 +0100

    Mostly documentation, clean-up code and type-hinting for exploregames.py

--- exploregames.py
+++ exploregames.py
@@ -1,20 +1,59 @@
+from typing import List, Tuple
 import streamlit as st
-import numpy as np
-import pandas as pd
 import altair as alt
+import pandas as pd
+import numpy as np
 
 SPACES = '&nbsp;' * 10
 
 
-def load_page(df, player_list):
-    selection_df, selected_game = prepare_layout(df)
-    plot_distribution(selection_df)
-    frequent_players(selection_df, player_list)
-    show_min_max_stats(selection_df, selected_game)
-    sidebar_activity_plot(selection_df)
+def load_page(df: pd.DataFrame,
+              player_list: List[str]) -> None:
+    """ In this section you can compare explore data for specific games.
+
+    Sections
+        * On the left you can see how often games were played in the last year of matches.
+        * You can see the total amount certain board games have been played.
+        * The longest break between board games.
+        * The longest chain of games played in days.
+        * The day most games have been played.
+
+    Parameters:
+    -----------
+
+    df : pandas.core.frame.DataFrame
+        The data to be used for the analyses of played board game matches.
+
+    player_list : list of str
+        List of players that participated in the board games
+    """
+
+    selected_game_df, selected_game = prepare_layout(df)
+    plot_distribution(selected_game_df)
+    plot_frequent_players(selected_game_df, player_list)
+    show_min_max_stats(selected_game_df, selected_game)
+    sidebar_activity_plot(selected_game_df)
 
 
-def prepare_layout(df):
+def prepare_layout(df: pd.DataFrame) -> Tuple[pd.DataFrame, str]:
+    """ Prepare layout and widgets
+
+    Parameters:
+    -----------
+
+    df : pandas.core.frame.DataFrame
+        The data to be used for the analyses of played board game matches.
+
+    Returns:
+    --------
+
+    selected_game_df : pandas.core.frame.DataFrame
+        Data filtered by the selected game
+
+    selected_game : str
+        The selected game
+    """
+
     st.title("🎲 Explore games")
     st.write("In this section you can explore the games that were played in the last year. "
              "Note that some games have different versions wich are needed to select in order to continue. "
@@ -32,23 +71,32 @@ def prepare_layout(df):
 
     # Select game and possibly a version of it
     selected_game = st.selectbox("Select a game to explore.", games)
-    selection = df.loc[(df.Game == selected_game), :]
-    versions = list(selection.Version.unique())
+    selected_game_df = df.loc[(df.Game == selected_game), :]
+    versions = list(selected_game_df.Version.unique())
     versions.sort()
     if len(versions) > 1:
         version = st.selectbox("Select a game to explore.", versions)
-        selection = selection.loc[selection.Version == version, :]
+        selected_game_df = selected_game_df.loc[selected_game_df.Version == version, :]
 
-    return selection, selected_game
+    return selected_game_df, selected_game
 
 
-def plot_distribution(df):
-    if sum(df.has_score.values) > 0:
+def plot_distribution(selected_game_df: pd.DataFrame) -> None:
+    """ Plot distribution of scores for a single board game
+
+    Parameters:
+    -----------
+
+    selected_game_df : pandas.core.frame.DataFrame
+        Data filtered by the selected game
+    """
+
+    if sum(selected_game_df.has_score.values) > 0:
         st.header("**♟** Distribution of Scores **♟**")
         st.write("Here, you can see the distribution of all scores that were achieved in the game. ")
 
-        score_selection_df = df.loc[:, [column for column in df.columns
-                                        if ('score' in column) & ('has_score' not in column)]]
+        score_selection_df = selected_game_df.loc[:, [column for column in selected_game_df.columns
+                                                      if ('score' in column) & ('has_score' not in column)]]
         game_scores = np.array(score_selection_df)
         game_scores = pd.DataFrame(game_scores[game_scores.nonzero()], columns=['Scores'])
 
@@ -60,10 +108,22 @@ def plot_distribution(df):
         st.altair_chart(chart)
 
 
-def show_min_max_stats(selection, selected_game):
-    score_selection = selection.loc[:, [column for column in selection.columns
-                                        if ('score' in column) & ('has_score' not in column)]]
+def show_min_max_stats(selected_game_df: pd.DataFrame,
+                       selected_game: str) -> None:
+    """ Show statistics for the worst and best players
 
+    Parameters:
+    -----------
+
+    selected_game_df : pandas.core.frame.DataFrame
+        Data filtered by the selected game
+
+    selected_game : str
+        The selected game
+    """
+
+    score_selection = selected_game_df.loc[:, [column for column in selected_game_df.columns
+                                               if ('score' in column) & ('has_score' not in column)]]
     score_matrix = np.array(score_selection)
 
     # Calculate average scores per player
@@ -100,7 +160,8 @@ def show_min_max_stats(selection, selected_game):
         st.header("**♟** Top players **♟**")
         st.write("Here are the best players for the game **{}**:".format(selected_game))
         st.write("{}🔹 Highest score by **{}** with {} points".format(SPACES, max_player, max_score))
-        st.write("{}🔸 Highest average score by **{}** with {} points".format(SPACES, high_avg_player, high_avg_player_val))
+        st.write("{}🔸 Highest average score by **{}** with {} points".format(SPACES, high_avg_player,
+                                                                              high_avg_player_val))
         st.write(" ")
 
         # Bottom players
@@ -110,12 +171,25 @@ def show_min_max_stats(selection, selected_game):
         st.write("{}🔸 Lowest average score by **{}** with {} points".format(SPACES, low_avg_player, low_avg_player_val))
 
 
-def frequent_players(selection_df, player_list):
+def plot_frequent_players(selected_game_df: pd.DataFrame,
+                          player_list: List[str]) -> None:
+    """ Show frequency of played games
+
+    Parameters:
+    -----------
+
+    selected_game_df : pandas.core.frame.DataFrame
+        Data filtered by the selected game
+
+    selected_game : str
+        The selected game
+    """
+
     st.header("**♟** Frequency of Matches **♟**")
     st.write("For each player, their total number of matches is displayed below.")
 
     # Calculate Frequencies
-    frequency = [len(selection_df.loc[selection_df[player + "_played"] == 1, :]) for player in player_list]
+    frequency = [len(selected_game_df.loc[selected_game_df[player + "_played"] == 1, :]) for player in player_list]
     frequency = pd.DataFrame(np.array([player_list, frequency]).T, columns=['Player', 'Frequency'])
 
     # Visualize Results
@@ -136,9 +210,18 @@ def frequent_players(selection_df, player_list):
     st.altair_chart(bars + text)
 
 
-def sidebar_activity_plot(selection_df):
-    selection_df = selection_df.sort_values("Date").set_index("Date").resample("3D").count().reset_index()
-    chart = alt.Chart(selection_df).mark_area(
+def sidebar_activity_plot(selected_game_df: pd.DataFrame) -> None:
+    """ Show frequency of played games over time
+
+    Parameters:
+    -----------
+
+    selected_game_df : pandas.core.frame.DataFrame
+        Data filtered by the selected game
+    """
+
+    selected_game_df = selected_game_df.sort_values("Date").set_index("Date").resample("3D").count().reset_index()
+    chart = alt.Chart(selected_game_df).mark_area(
         color='goldenrod',
         opacity=1
     ).encode(
